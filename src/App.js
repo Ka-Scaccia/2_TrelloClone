5bb82afc6e3340107063c27e00b037a3a55acae0
Katia <katiascsccia@gmail.com>
2026-06-05 16:15:34 +0200

    inserito contenitore per la scheda

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import "./CSS/generalEroot.css";
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="App container">
+      {/* Scheda */}
+      <div className="scheda"></div>
     </div>
   );
 }
