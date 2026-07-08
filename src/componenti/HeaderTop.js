c7856b8345d0d8015dfb5916c5464e7110cd0753
Katia <katiascsccia@gmail.com>
2026-07-08 17:42:59 +0200

    sistemato file HeaderTop.js

--- src/componenti/HeaderTop.js
+++ src/componenti/HeaderTop.js
@@ -1,12 +1,12 @@
 import "../CSS/header.css";
 import "../CSS/utily.css";
-import iconaTrello from "../assest/icons/iconaTrello.svg";
+import logoTrello from "../assest/icons/logoTrello.svg";
 
 export const HeaderTop = () => {
   return (
     <>
       <div className="headerTop flex">
-        <img src={iconaTrello} alt="icona Trello" />
+        <img src={logoTrello} alt="logo Trello" />
         <h1>Clone Trello</h1>
       </div>
     </>
